211377f592a4e4a0ca3e44081b98f2f59e34c57f
Richard <skynet.tw@gmail.com>
2022-10-16 11:32:18 +0800

    lotto number prediction

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,10 @@
 from re import L
 from django.shortcuts import render
+import random
 
 def index(request):
     name = "Richard"
+    numbers = list(range(1, 50))
+    random.shuffle(numbers)
+    lotto = sorted(numbers[:6])
     return render(request, "index.html", locals())
